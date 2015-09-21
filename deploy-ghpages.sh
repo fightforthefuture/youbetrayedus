680500d11246f66318fea7176e9de543cd3fb08e
Jeff Lyon <jeff@rubbingalcoholic.com>
2015-09-21 12:40:01 -0700

    trying this

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -3,14 +3,15 @@ cd dist || exit 0;
 
 if  [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST" = "false" ]
   then
-    (git init
+    (cp ../CNAME ./CNAME
+     git init
      git config user.name "Travis-CI"
      git config user.email "travis@example.org"
      git add .
      git commit -m "Deployed to Github Pages"
-     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+     git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
     )
 fi
 
-# Move this to L6 once there's a CNAME file
-cp ../CNAME ./CNAME
+
+
